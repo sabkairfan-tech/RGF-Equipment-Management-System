437a5c742a20f09ff961fc762c3c3c88682650a6
Irfan Hanif <sabkairfan@gmail.com>
2026-06-21 14:00:51 +0300

    checking

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -2,9 +2,32 @@ import { useState } from "react";
 
 export default function Test() {
 
-    const [model , setModel] = useState(false);
+    const [showModel , setModel] = useState(false);
+
+    const [editId, setEditId] = useState(null);
 
 
+const [form , setForm] = useState({
+    serialNo: "",
+    name: "",
+    category: "",
+    status: "Available",
+    
+    
+});
+
+// Open Edit Modal
+  const openEditModal = (item) => {
+    setForm(item);
+    setEditId(item.id);
+    setModal(true);
+  };
+
+  const openAddModal = () => {
+    setForm({ serialNo: "", name: "", category: "", status: "Available" });
+    setEditId(null);
+    setModel(true);
+  };
     
   return (
 <>
@@ -12,7 +35,9 @@ export default function Test() {
 
 <div className="flex justify-between items-center mb-4">
         <h2 className="text-xl font-bold">Test</h2>
-
+<button className="bg-blue-600 text-white px-4 py-2 rounded" onClick={openAddModal}>
+Assign Equipment
+</button>
 
 </div>
 
@@ -55,7 +80,71 @@ export default function Test() {
 </table>
 
 </div>
+ {/* MODAL */}
+      {showModel && (
+        <div className="fixed inset-0  flex items-center justify-center">
+          <div className="bg-white p-6 rounded w-400">
+            <h2 className="text-xl font-bold mb-4">
+              {editId ? "Edit Equipment" : "Add Equipment"}
+            </h2>
 
+            <input
+              className="w-xl border p-2 mb-2"
+              placeholder="Serial No"
+              value={form.serialNo}
+              onChange={(e) =>
+                setForm({ ...form, serialNo: e.target.value })
+              }
+            />
+
+            <input
+              className="w-xl  border p-2 mb-2"
+              placeholder="Name"
+              value={form.name}
+              onChange={(e) =>
+                setForm({ ...form, name: e.target.value })
+              }
+            />
+
+            <input
+              className="w-xl  border p-2 mb-2"
+              placeholder="Category"
+              value={form.category}
+              onChange={(e) =>
+                setForm({ ...form, category: e.target.value }) 
+              }
+            />
+
+            <select
+              className="w-xl  border p-2 mb-4"
+              value={form.status}
+              onChange={(e) =>
+                setForm({ ...form, status: e.target.value })
+              }
+            >
+              <option>Available</option>
+              <option>Assigned</option>
+              <option>Repair</option>
+            </select>
+
+            <div className="flex justify-end gap-2">
+              <button
+                onClick={() => setModel(false)}
+                className="px-4 py-2 bg-gray-300 rounded"
+              >
+                Cancel
+              </button>
+
+              <button
+               
+                className="px-4 py-2 bg-blue-600 text-white rounded"
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
 </>
 
 
